ed4bfd8474debb06da0ca4e14982d7e0f36a5db0
Samuli Seppänen <samuli.seppanen@gmail.com>
2014-11-20 18:09:15 +0200

    Fixes to rdoc
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,16 @@
 #
 # Install and configure Git
 #
+# == Authors
+#
+# Samuli Seppänen <samuli.seppanen@gmail.com>
+#
+# Samuli Seppänen <samuli@openvpn.net>
+#
+# == License
+#
+# BSD-license. See file LICENSE for details.
+#
 class git {
 
 # Rationale for this is explained in init.pp of the sshd module
